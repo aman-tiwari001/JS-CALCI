9b3d44c22b9142308bc3f8748e25a41449cbfbce
Aman Tiwari <83050165+aman-tiwari001@users.noreply.github.com>
2023-06-17 13:49:18 +0530

    updated script.js

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ Array.from(btnList).forEach((button)=>{
     button.addEventListener('click', (e)=>{
         if(e.target.innerText=='=') {
             try {
+                text = text.replace('%', '/100'); // Replace % with /100 to allow evaluation
                 text = eval(text);
                 display.value = text;
             } catch (error) {
                 text = "";
                 display.value = "Invalid Expression";
             }
-            
         }
         else if(e.target.innerText=='C') {
             text = "";
@@ -25,7 +25,9 @@ Array.from(btnList).forEach((button)=>{
             display.value = text;
         }
         else if(e.target.innerText=='%') {
-            
+            // Append the '%' sign to the text
+            text = text + '%';
+            display.value = text;
         }
         else {
             text = text + e.target.innerText;
@@ -33,4 +35,3 @@ Array.from(btnList).forEach((button)=>{
         }
     });
 });
-
